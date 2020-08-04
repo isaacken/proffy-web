9124f603471cc348e3568043c92be0d8a1c3fa85
Isaac Kennedy <isaackennedyfds@gmail.com>
2020-08-04 05:49:32 -0300

    Updates

---
+++ precache-manifest.2d09b5618c6133b3fc3d22e527137b56.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8e6b2fe7f4d84b1680ce7c89aa5999",
+    "url": "/proffy/index.html"
+  },
+  {
+    "revision": "d453eb28e482aab6598c",
+    "url": "/proffy/static/css/main.3e14b477.chunk.css"
+  },
+  {
+    "revision": "fef726eff63d9adc3f10",
+    "url": "/proffy/static/js/2.3c201022.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/proffy/static/js/2.3c201022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d453eb28e482aab6598c",
+    "url": "/proffy/static/js/main.cda7a91a.chunk.js"
+  },
+  {
+    "revision": "710ad84d386634e76f4d",
+    "url": "/proffy/static/js/runtime-main.50fd472a.js"
+  },
+  {
+    "revision": "dc8c47fa945838a208074157d997436d",
+    "url": "/proffy/static/media/back.dc8c47fa.svg"
+  },
+  {
+    "revision": "156102ee75a899123483211bbf4a8fbd",
+    "url": "/proffy/static/media/give-classes.156102ee.svg"
+  },
+  {
+    "revision": "211f2bb00860f91804cfb2354e17b255",
+    "url": "/proffy/static/media/landing.211f2bb0.svg"
+  },
+  {
+    "revision": "86fef799b0726c66fb7a9d710e2d922c",
+    "url": "/proffy/static/media/logo.86fef799.svg"
+  },
+  {
+    "revision": "bfdb3ebe513b5b2411a5014d6aad3abe",
+    "url": "/proffy/static/media/purple-heart.bfdb3ebe.svg"
+  },
+  {
+    "revision": "e7a7e1b4e016245fa97e6f42aee4bcaa",
+    "url": "/proffy/static/media/study.e7a7e1b4.svg"
+  },
+  {
+    "revision": "45c6e6ecebde5ee441644d41cc132ce4",
+    "url": "/proffy/static/media/whatsapp.45c6e6ec.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.853fd73fb848a0a99a3a3dbec6dd1c8a.js"
+  "/proffy/precache-manifest.2d09b5618c6133b3fc3d22e527137b56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/proffy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
