80e98ca03ecfdd70c3a37d49adbaf9015a973944
Isaac Kennedy <isaac@postspot.co>
2020-08-04 00:26:30 -0300

    NLW first day

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem () {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://pbs.twimg.com/profile_images/1254962201686933504/IcQyArlU_400x400.jpg" alt="Isaac Kennedy"/>
+                <div>
+                    <strong>Isaac Kennedy</strong>
+                    <span>Matemática</span>
+                </div>
+            </header>
+
+            <p>
+                Doutorado pela Universidade de Harvard em Matemática na área de fazer as contas de quanto tempo procrastina por dia.
+                <br /><br />
+                Não sabe a tabuada do 7 completa, mas sabe somar quaisquer números de até 1 dígito.
+            </p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ 12,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp"/>
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem;
