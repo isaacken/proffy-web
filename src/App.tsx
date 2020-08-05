c45e1a515a3114af4a403a748464b6c49cf27121
Isaac Kennedy <isaackennedyfds@gmail.com>
2020-08-05 16:53:07 -0300

    Identation update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './assets/styles/global.css';
 
 function App() {
   return (
-    <Routes />
+  <Routes />
   );
 }
 
--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -5,34 +5,34 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
 
 function TeacherItem () {
-    return (
-        <article className="teacher-item">
-            <header>
-                <img src="https://pbs.twimg.com/profile_images/1254962201686933504/IcQyArlU_400x400.jpg" alt="Isaac Kennedy"/>
-                <div>
-                    <strong>Isaac Kennedy</strong>
-                    <span>Matemática</span>
-                </div>
-            </header>
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://pbs.twimg.com/profile_images/1254962201686933504/IcQyArlU_400x400.jpg" alt="Isaac Kennedy"/>
+        <div>
+          <strong>Isaac Kennedy</strong>
+          <span>Matemática</span>
+        </div>
+      </header>
 
-            <p>
-                Doutorado pela Universidade de Harvard em Matemática na área de fazer as contas de quanto tempo procrastina por dia.
-                <br /><br />
-                Não sabe a tabuada do 7 completa, mas sabe somar quaisquer números de até 1 dígito.
-            </p>
+      <p>
+        Doutorado pela Universidade de Harvard em Matemática na área de fazer as contas de quanto tempo procrastina por dia.
+        <br /><br />
+        Não sabe a tabuada do 7 completa, mas sabe somar quaisquer números de até 1 dígito.
+      </p>
 
-            <footer>
-                <p>
-                    Preço/hora
-                    <strong>R$ 0,30</strong>
-                </p>
-                <button type="button">
-                    <img src={whatsappIcon} alt="Whatsapp"/>
-                    Entrar em contato
-                </button>
-            </footer>
-        </article>
-    );
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 0,30</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp"/>
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
 }
 
 export default TeacherItem;
